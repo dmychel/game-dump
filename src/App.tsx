ebc44cc31936490499a3048e35f1e368788cf197
dylon <dmychel@tutanota.com>
2024-05-12 22:42:39 -0400

    import and rendered home and nav component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./routes/Index";
 import Error from "./routes/Error";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Nav />
     </BrowserRouter>
   );
 }
