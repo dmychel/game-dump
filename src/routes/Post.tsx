0ce67c067fb5cbd958b2022bb54b0086b4b01adb
dmychel <dmychel@tutanota.com>
2024-05-14 18:55:39 -0400

    created component

---
+++ src/routes/Post.tsx
@@ -0,0 +1,10 @@
+export default function Post() {
+  return (
+    <div className="new-post">
+      <form>
+        <input type="text" placeholder="Text here" />
+      </form>
+      <button>Add file</button>
+    </div>
+  );
+}
