b2e20f09bef63e7103582e20e5a69ec9aeed6321
dmychel <dmychel@tutanota.com>
2024-05-13 20:02:06 -0400

    created profile route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Index from "./routes/Index";
 import Error from "./routes/Error";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Profile from "./routes/Profile";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </main>
