021c23b527e0db64b0238634df9b2cb9066a162b
dmychel <dmychel@tutanota.com>
2024-05-17 19:35:26 -0400

    removed class names temporarily

--- src/components/profile/Games.tsx
+++ src/components/profile/Games.tsx
@@ -1,13 +1,13 @@
 export default function Games() {
   return (
-    <div className="games content">
-      <div className="game-img">
+    <div>
+      <div>
         <img src="https://fakeimg.pl/1600x900" alt="" />
       </div>
-      <div className="game-title">
+      <div>
         <h2>Game Title</h2>
       </div>
-      <div className="game-hours">
+      <div>
         <h3>90 hours</h3>
       </div>
     </div>
