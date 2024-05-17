07bba8095dc2ac3c4f9e3a7359e4977d155a65b6
dmychel <dmychel@tutanota.com>
2024-05-17 19:34:56 -0400

    jsx change

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,11 +1,12 @@
 import Post from "../components/Post";
+import styles from "/src/styles/modules/index.module.css";
 
 export default function Index() {
   return (
-    <div className="home content">
+    <section className={styles.home}>
       <Post type={true} />
       <Post type={false} />
       <Post type={true} />
-    </div>
+    </section>
   );
 }
