5694d464e4dcf9a74b353cbdcf0ab4c570a1815c
dmychel <dmychel@tutanota.com>
2024-05-17 19:35:05 -0400

    jsx change

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,26 +1,19 @@
 import { Link } from "react-router-dom";
-import "/src/styles/nav.module.css";
 
 export default function Nav() {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">
-            <img src="/assets/house.png" />
-          </Link>
-        </li>
-        <li>
-          <Link to="/post">
-            <img src="/assets/more.png" />
-          </Link>
-        </li>
-        <li>
-          <Link to="/profile">
-            <img src="/assets/user.png" />
-          </Link>
-        </li>
-      </ul>
+      <Link to="/">
+        <img src="/assets/house.png" />
+      </Link>
+
+      <Link to="/post">
+        <img src="/assets/more.png" />
+      </Link>
+
+      <Link to="/profile">
+        <img src="/assets/user.png" />
+      </Link>
     </nav>
   );
 }
