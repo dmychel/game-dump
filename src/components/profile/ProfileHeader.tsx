cc61c3f99967adb86466eab0206e2be4cf7725cf
dmychel <dmychel@tutanota.com>
2024-05-19 16:48:45 -0400

    background jsx for header

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,33 +1,11 @@
+import styles from "/src/styles/modules/profile/profileHeader.module.css";
+
 export default function ProfileHeader() {
   return (
-    <>
-      <div>
+    <div className={styles.header}>
+      <div className={styles.profile_background}>
         <img src="https://fakeimg.pl/1600x900" alt="" />
       </div>
-      <div>
-        <div>
-          <div>
-            <img src="https://fakeimg.pl/500x500" alt="" />
-          </div>
-
-          <div>
-            <h3>Username</h3>
-            <p>Online</p>
-          </div>
-          <div>
-            <img src="https://fakeimg.pl/1000x500" alt="" />
-          </div>
-        </div>
-        <div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus
-            illum explicabo labore deserunt ab exercitationem error accusantium
-            dolor. Qui aut blanditiis est unde ipsa ipsam aspernatur voluptate
-            saepe libero et!
-          </p>
-          <button>Edit Profile</button>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
