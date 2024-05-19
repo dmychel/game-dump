4a30da8d005c4aa2ee52db8872a4fd768fbb986a
dmychel <dmychel@tutanota.com>
2024-05-19 17:38:30 -0400

    jsx update

--- src/components/profile/Games.tsx
+++ src/components/profile/Games.tsx
@@ -1,14 +1,13 @@
+import styles from "/src/styles/modules/profile/userGames.module.css";
+
 export default function Games() {
   return (
-    <div>
-      <div>
+    <div className={styles.game}>
+      <div className={styles.game_image}>
         <img src="https://fakeimg.pl/1600x900" alt="" />
       </div>
-      <div>
+      <div className={styles.game_title}>
         <h2>Game Title</h2>
-      </div>
-      <div>
-        <h3>90 hours</h3>
       </div>
     </div>
   );
