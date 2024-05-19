f56cd22c3d2b599dd5b5dbc973cb6ccb5da77daf
dmychel <dmychel@tutanota.com>
2024-05-19 17:38:19 -0400

    created component

--- src/components/profile/UserGames.tsx
+++ src/components/profile/UserGames.tsx
@@ -1,5 +1,19 @@
 import Games from "./Games";
+import styles from "/src/styles/modules/profile/userGames.module.css";
+import { Link } from "react-router-dom";
 
 export default function ProfileGames() {
-  return;
+  return (
+    <div className={styles.user_games}>
+      <div className={styles.games_header}>
+        <h3>Users Games</h3>
+        <Link to="/profile">32 Games</Link>
+      </div>
+      <div className={styles.games}>
+        <Games />
+        <Games />
+        <Games />
+      </div>
+    </div>
+  );
 }
