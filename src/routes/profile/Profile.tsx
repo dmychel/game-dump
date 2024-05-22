5c70151b0c8cd84471db63c8184863b2b0695bc9
dmychel <dmychel@tutanota.com>
2024-05-22 16:25:25 -0400

    moved file location to folder

---
+++ src/routes/profile/Profile.tsx
@@ -0,0 +1,13 @@
+import ProfileHeader from "../../components/profile/ProfileHeader";
+import UserInfo from "../../components/profile/UserInfo";
+import UserGames from "../../components/profile/UserGames";
+
+export default function Profile() {
+  return (
+    <section className="profile">
+      <ProfileHeader />
+      <UserInfo />
+      <UserGames />
+    </section>
+  );
+}
